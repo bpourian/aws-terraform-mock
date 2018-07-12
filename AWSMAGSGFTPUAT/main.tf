d75adfbcd103885977b0a0536a2f83b9612f4663
bpourian <b.pourian@hotmail.co.uk>
2018-07-12 12:13:17 +0100

    Updated main file for PUAT and stack tested green

--- AWSMAGSGFTPUAT/main.tf
+++ AWSMAGSGFTPUAT/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
     cidr_block          = "${var.vpc_cidr_block}"
-    instance_tenancy    = "dedicated"
+    instance_tenancy    = "default"
 
     tags {
       Name               = "${var.vpc_name}"
@@ -37,8 +37,8 @@ resource "aws_route_table" "main" {
     vpc_id = "${aws_vpc.main.id}"
 
     route {
-      cidr_block             = "${var.route_cidr_block}"
-      gateway_id             = "${aws_internet_gateway.main.id}"
+      cidr_block         = "${var.route_cidr_block}"
+      gateway_id         = "${aws_internet_gateway.main.id}"
     }
 
     tags {
@@ -53,8 +53,10 @@ resource "aws_main_route_table_association" "main" {
 }
 
 resource "aws_subnet" "main" {
-    vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.subnet_cidr_block}"
+    vpc_id                  = "${aws_vpc.main.id}"
+    cidr_block              = "${var.subnet_cidr_block}"
+    availability_zone       = "eu-west-2a"
+    map_public_ip_on_launch = true
 
     tags {
       Name               = "${var.subnet_name}"
@@ -78,24 +80,13 @@ resource "aws_network_interface" "main" {
     }
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = "${aws_security_group.allow_rdp.id}"
-  network_interface_id = "${aws_network_interface.main.id}"
-}
-
-resource "aws_eip" "one" {
-    vpc                       = true
-    network_interface         = "${aws_network_interface.main.id}"
-    associate_with_private_ip = "10.0.0.4"
-}
-
 data "aws_ami" "amazon_windows_2016" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["Windows_Server-2016*", "Base"]
+    values = ["Windows_Server-2016-English-Full-Base-2018.06.13"]
   }
 
   filter {
@@ -114,16 +105,14 @@ resource "aws_instance" "winserv2016" {
     timeout = "10m"
   }
 
-
     ami             = "${data.aws_ami.amazon_windows_2016.image_id}"
-    # ami             = "ami-479b7520"
     instance_type   = "t2.micro"
-    # security_groups = ["${aws_security_group.allow_rdp.name}"]
     subnet_id       = "${aws_subnet.main.id}"
     key_name        = "${var.key_name}"
+    security_groups = ["${aws_security_group.allow_rdp.id}"]
 
     tags {
       Name               = "${var.machine_name}"
       Resource_Group_VPC = "${var.resource_group_ec2}"
-  }
+    }
 }
